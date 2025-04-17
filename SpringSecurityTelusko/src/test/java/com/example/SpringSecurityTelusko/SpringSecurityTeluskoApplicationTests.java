75ca6aa1e282eed45cb493d2ccfbc41459245b75
Harsh Tripathi <harshtripathi042@gmail.com>
2025-04-17 23:21:11 +0530

    server setup with security

---
+++ SpringSecurityTelusko/src/test/java/com/example/SpringSecurityTelusko/SpringSecurityTeluskoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringSecurityTelusko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTeluskoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
