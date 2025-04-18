2429cb7e5dae7f18a4d864c0026f631e39aa8c8e
Harsh Tripathi <harshtripathi042@gmail.com>
2025-04-18 12:47:42 +0530

    login done without jwt

--- SpringSecurityTelusko/src/main/java/com/example/SpringSecurityTelusko/config/SecurityConfig.java
+++ SpringSecurityTelusko/src/main/java/com/example/SpringSecurityTelusko/config/SecurityConfig.java
@@ -3,9 +3,11 @@ package com.example.SpringSecurityTelusko.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -51,7 +53,7 @@ public class SecurityConfig {
 
                 // Route access control
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers("/auth/**").permitAll() // Allow public access to /auth routes
+                        .requestMatchers("register","login").permitAll() // Allow public access to /auth routes
                         .anyRequest().authenticated()            // All other routes require login
                 )
 
@@ -73,4 +75,10 @@ public class SecurityConfig {
         // that Spring Security will use to protect the application.
         return httpSecurity.build();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
+        return config.getAuthenticationManager();
+    }
+
 }
--- SpringSecurityTelusko/src/main/java/com/example/SpringSecurityTelusko/controllers/UserController.java
+++ SpringSecurityTelusko/src/main/java/com/example/SpringSecurityTelusko/controllers/UserController.java
@@ -3,6 +3,10 @@ package com.example.SpringSecurityTelusko.controllers;
 import com.example.SpringSecurityTelusko.entity.User;
 import com.example.SpringSecurityTelusko.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,9 +18,23 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     @PostMapping("register")
     public User register(@RequestBody User user){
         return userService.saveUser(user);
     }
 
+    @PostMapping("login")
+    public String login(@RequestBody User user){
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+
+        if(authentication.isAuthenticated()){
+            return "Sucess";
+        }
+        else
+            return "login failed";
+    }
 }
